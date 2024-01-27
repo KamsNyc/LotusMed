6b6ad7a9a7991b1b15c3289445a84894afac35fe
Kamsnyc <kamel.singh1999@gmail.com>
2024-01-27 17:36:12 -0500

    updated background colors for videos

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
 
   //return
   return (
-    <main className="w-full h-screen bg-[#170B3B]">
+    <main className="w-full h-screen bg-[#100823]">
       {showSecondVideo ? (
         <SecondVideo onVideoEnd={handleSecondVideoEnd} />
       ) : (
